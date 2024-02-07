15b339284116c0d38482da4b1d96866629a5018c
ELCBAh <6211@holbertonstudents.com>
2024-02-07 19:42:39 +0000

    checking checker error

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -13,5 +13,4 @@ rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
 
 rl.on('close', () => {
   console.log('This important software is now closing');
-  process.exit(0);
 });
