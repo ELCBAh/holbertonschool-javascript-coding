9a6ac2fd0a9359245ff41709216ecbbeff4ec72c
ELCBAh <6211@holbertonstudents.com>
2024-02-07 19:45:11 +0000

    fixed little misunderstanding

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -7,7 +7,7 @@ const rl = readline.createInterface({
 });
 
 rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
-  stdout.write('Your name is: ' + name + '\r');
+  console.log('Your name is: ' + name);
   rl.close();
 });
 
